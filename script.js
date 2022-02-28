1971a7cf3c04d75626f2371a727b03f743f8d900
Alexandra Davis <alex.davis806@gmail.com>
2022-02-28 13:00:49 -0700

    new styling

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Hello there! My name is Alex and I am from Texas.";
 document.querySelector("body").appendChild(h2);
+
+
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+  input.innerHTML = "hello friend!"
+});
+
